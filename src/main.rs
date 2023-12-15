eb435ad628b658b9c8536f30b64567f9105bf6a7
cecarbs <carbonel257@gmail.com>
2023-12-14 21:37:39 -0600

    Added temp method to search file tree (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{Args, Parser, Subcommand};
 // use indicatif::{ProgressBar, ProgressStyle, Style};
 use indicatif::{ProgressBar, ProgressStyle};
-use nucleo::Matcher;
+use nucleo::{Matcher, Config};
 use std::fs::{read_dir, DirEntry};
 use std::path::PathBuf;
 use std::u64;
@@ -75,3 +75,42 @@ fn main() {
         }
     }
 }
+
+fn fuzzy_find_dirs_recursive(pattern: &str) -> Vec<String> {
+    // let config = Config::pattern(pattern).case_sensitive(true);
+    let config = Config::DEFAULT;
+    let mut results = Vec::new();
+    let matcher = Matcher::new(config);
+
+    // Start at the root directory
+    let mut current_path = std::path::PathBuf::from("/");
+
+    loop {
+        for entry in read_dir(&current_path).unwrap() {
+            let entry = entry.unwrap();
+            let entry_type = entry.metadata().unwrap().file_type();
+
+            // Check if it's a directory and not a symbolic link
+            if entry_type.is_dir() && !entry_type.is_symlink() {
+                let name = entry.path().display().to_string();
+                if matcher.match_score(&name) > 0 {
+                    results.push(name)
+                }
+
+                // Recursively search inside the directory
+                current_path.push(entry.file_name());
+                fuzzy_find_dirs_recursive(pattern);
+
+                // Go back up on level
+                current_path.pop();
+                }
+            }
+            // Break out of the loop when no more directories to explore
+            if let std::result::Result::Ok(None) = read_dir(&current_path) {
+                break;
+            }
+        }
+
+    results
+}
+
